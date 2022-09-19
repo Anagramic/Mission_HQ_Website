dd77b1a204c99b6724e0fa4b6ef57b52fd76c830
Anagramic <52174878+Anagramic@users.noreply.github.com>
2022-09-19 16:10:46 +0100

    Add files via upload

---
+++ Code/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<link rel="stylesheet" href="styles.css">
+<html>
+        <h1>Welcome!</h1>
+    <body>
+        <p>Welcome to Mission HQ</p>
+        <br>
+        <p>To get started log in here</p>
+        <form action="login.php" method="post">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Log in">
+        <br>
+        <br>
+        <br>
+        <p>Don't have an acount yet? No problem! Simply click here:</p> 
+        <a href="create.php"> Create new acount </a>
+    </body>
+</html>
